1e0d5368a023c0e0a87c3253e9b7cbb214797b65
faed <327399770@qq.com>
2025-09-17 17:46:20 +0800

    test

--- src/ModelHelper.php
+++ src/ModelHelper.php
@@ -3,11 +3,68 @@
 namespace Faed\HyperfSaasHelper;
 
 use Hyperf\Conditionable\HigherOrderWhenProxy;
-use Hyperf\Context\Context;
 use Hyperf\Database\Model\Builder;
-use Hyperf\DbConnection\Model\Model as BaseModel;
-
+/**
+ * @method static Builder search($value, $field, $operation = '=')
+ * @method static Builder searchIn($value, $field, $operation = '=')
+ * @method static Builder searchLike($value,$field)
+ * @method static Builder searchTime($startTime = null , $endTime = null, string $field = 'created_at')
+ * @method static Builder fieldDate($value,string $field = 'created_at')
+ * @method static Builder startTime($value,string $field = 'created_at')
+ * @method static Builder endTime($value,string $field = 'created_at')
+ */
 trait ModelHelper
 {
+    public function scopeSearch(Builder $builder, $value, $field, string $operation='='): HigherOrderWhenProxy|Builder
+    {
+        return $builder->when($value,function (Builder $builder) use ($value,$field,$operation){
+            $builder->where($field,$operation,$value);
+        });
+    }
 
+    public function scopeSearchIn(Builder $builder, $value, $field): HigherOrderWhenProxy|Builder
+    {
+        return $builder->when($value,function (Builder $builder) use ($value,$field){
+            if (is_array($value)){
+                $builder->whereIn($field,$value);
+            }else{
+                $builder->whereIn($field,explode(',',$value));
+            }
+        });
+    }
+
+    public function scopeSearchLike(Builder $builder,$value,$field): HigherOrderWhenProxy|Builder
+    {
+        return $builder->when($value,function (Builder $builder,$value) use ($field){
+            $builder->where($field,'like',"%{$value}%");
+        });
+    }
+
+    public function scopeSearchTime(Builder $builder,$startTime = null , $endTime = null, string $field = 'created_at')
+    {
+        return $builder->startTime($startTime,$field)->endTime($endTime,$field);
+    }
+
+    public function scopeStartTime(Builder $builder,$value, string $field = 'created_at'): HigherOrderWhenProxy|Builder
+    {
+        return $builder->when($value,function (Builder $builder,$value) use ($field){
+            $builder->where($field,'>=',$value);
+        });
+    }
+
+
+
+    public function scopeEndTime(Builder $builder,$value, string $field = 'created_at'): HigherOrderWhenProxy|Builder
+    {
+        return $builder->when($value,function (Builder $builder,$value) use ($field){
+            $builder->where($field,'<=',$value);
+        });
+    }
+
+    public function scopeFieldDate(Builder $builder, $value,string $field = 'created_at'): HigherOrderWhenProxy|Builder
+    {
+        return $builder->when($value,function (Builder $builder) use ($value,$field){
+            $builder->whereDate($field,$value);
+        });
+    }
 }
