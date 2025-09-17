9632776a73d99035136d602e73dc618893e0cc25
faed <327399770@qq.com>
2025-09-17 16:26:02 +0800

    test

--- src/Helper.php
+++ src/Helper.php
@@ -124,7 +124,6 @@ if (!function_exists('multiGroupSum')){
 
             // 更新求和字段和计数
             foreach ($sumFields as $sumField) {
-//                $result[$groupKey][$sumField] += floatval($item[$sumField] ?? 0);
                 $result[$groupKey][$sumField] = FluentCalculator::init($result[$groupKey][$sumField])->add(floatval($item[$sumField] ?? 0))->getResult();
             }
             $result[$groupKey]['count']++;
