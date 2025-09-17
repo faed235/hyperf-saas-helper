48d34011ef54d26ece9de3f67669ac691080ab55
faed <327399770@qq.com>
2025-09-17 17:20:47 +0800

    test

---
+++ src/ModelHelper.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Faed\HyperfSaasHelper;
+
+use Hyperf\Conditionable\HigherOrderWhenProxy;
+use Hyperf\Context\Context;
+use Hyperf\Database\Model\Builder;
+use Hyperf\DbConnection\Model\Model as BaseModel;
+
+trait ModelHelper
+{
+
+}
